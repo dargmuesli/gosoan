568e3a7a53d636971a3419fd9ac09353350260fd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-03-16 04:54:37 +0100

    feat: add initial content

---
+++ app/src/main/java/de/jonas_thelemann/uni/gosoan/App.kt
@@ -0,0 +1,6 @@
+package de.jonas_thelemann.uni.gosoan
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/de/jonas_thelemann/uni/gosoan/ui/MainFragment.kt
@@ -0,0 +1,13 @@
+package de.jonas_thelemann.uni.gosoan.ui
+
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+
+class MainFragment : Fragment() {
+}
